a1db2970b7c905f4d5a630dcbdaa8f158c00c084
Anita Kumari <anitajnv15@gmail.com>
2026-06-09 11:52:49 +0530

    adding javascript files

---
+++ 03.js
@@ -0,0 +1,79 @@
+// Datatype conversion
+
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score = 'anita'
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// '33' -> 33
+// '33abc' -> NaN (not a number)
+// true -> 1; false -> 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn)
+
+// let isLoggedIn = ""
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn)
+
+
+// let isLoggedIn = "anita"
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn)
+
+//1 -> true; 0 -> false
+// "" -> false
+// "ani" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
